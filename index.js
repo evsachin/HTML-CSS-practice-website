ca7a5da694aacba56994485e8b7c9c8c45f4edcf
wtsachin <sachin.kolhe.evisiontech@gmail.com>
2025-01-09 05:13:10 +0530

    feat: added file structure

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+var num = 20
+let num2 = 3.14
+const pi = 3.14
+
+function handleOperators(){
+    // Arithmetic Operators
+    console.log(a+b)
+    console.log(a-b)
+    console.log(a*b)
+    console.log(a/b)
+    console.log(a%b)
+
+    // 
+}
+
+
+function ShowLoops(){
+    if (a + b) 
+        return a
+    else{
+        console.log('in else...')
+    }
+}
+
+function showSwitchCase(){
+    switch(a + b){
+        case x:
+            break;
+        case y:
+            //
+            break;
+        default : 
+            //
+
+    }
+}
+
+
+// function declaration
+function calcRectArea(width, height) {
+  return width * height;
+}
+
+const myFunc = () => {
+    console.log('arrow function')
+}
